eee14b331a178a15baa5a3b107efd27c835a506d
Daniel Weinmann <danielweinmann@gmail.com>
2013-04-07 03:41:44 -0300

    Updating counters

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,8 +4,8 @@ class CoursesController < ApplicationController
     @deadline = Time.parse('2013-04-15 23:59:59 -0300')
     @remaining_days = (@deadline.to_date - Time.now.to_date).to_i
     @remaining_days = 0 if @remaining_days < 0
-    @leaders_to_go = 5
-    @followers_to_go = 5
+    @leaders_to_go = 4
+    @followers_to_go = 4
   end
   
 end
