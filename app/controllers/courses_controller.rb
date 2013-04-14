5233e6079455e1519a1c8e8d1732d82eade478e7
Daniel Weinmann <danielweinmann@gmail.com>
2013-04-14 11:07:54 -0300

    Fixing deadline

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,7 +1,7 @@
 class CoursesController < ApplicationController
   
   def index
-    @deadline = Time.parse('2013-04-15 23:59:59 -0300')
+    @deadline = Time.parse('2013-04-14 23:59:59 -0300')
     @remaining_days = (@deadline.to_date - Time.now.to_date).to_i
     @remaining_days = 0 if @remaining_days < 0
     @leaders_to_go = 2
