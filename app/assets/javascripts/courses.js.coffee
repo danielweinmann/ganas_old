2f280a31f52a23f0066a03bd251380d14e2b2db1
Daniel Weinmann <danielweinmann@gmail.com>
2013-05-04 12:06:30 -0300

    Adding fun scrolling to the navigation

--- app/assets/javascripts/courses.js.coffee
+++ app/assets/javascripts/courses.js.coffee
@@ -1,3 +1,16 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+$(document).ready () ->
+
+  block = $('.block').get(0)
+  backLink = $('#back_to_top')
+
+  $('.anchor').anchorAnimate({ topOffset: 0 })
+  
+  backLink.topLink({ min: 200, fadeSpeed: 500 })
+  
+  backLink.click (event) ->
+    event.preventDefault()
+    $.scrollTo(0, 300)
---
+++ app/assets/javascripts/lib/jquery.anchor.js
@@ -0,0 +1,64 @@
+/*******
+
+	***	Anchor Slider by Cedric Dugas   ***
+	*** Http://www.position-absolute.com ***
+	
+	Never have an anchor jumping your content, slide it.
+
+	Don't forget to put an id to your anchor !
+	You can use and modify this script for any project you want, but please leave this comment as credit.
+	
+*****/
+
+//$(document).ready(function () {
+	//$("a.anchorLink").anchorAnimate();
+//});
+
+jQuery.fn.anchorAnimate = function(settings) {
+ 	settings = jQuery.extend({ speed: 500, topOffset: 0 }, settings);	
+    this.click(function (e) {
+        var locationHref, elementClick, destination;
+
+        //if (navigator.appName === 'Microsoft Internet Explorer') {
+            //settings.topOffset = 0;
+        //} else {
+            //e.preventDefault();
+            //e.stopPropagation();
+        //}
+
+        //locationHref = window.location.href;
+        elementClick = $(this).attr("href");
+        destination = $(elementClick).offset().top + settings.topOffset;
+
+        // O callback depois da animação servia para adicionar o hash ao endereço,
+        // isso fazia o pulo acontecer, coisa que eu não gostei.
+        $("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination }, settings.speed);
+
+        return false;
+    });
+    return this;
+};
+
+//plugin scroll to top
+jQuery.fn.topLink = function(settings) {
+	settings = jQuery.extend({
+		min: 1,
+		fadeSpeed: 200
+	}, settings);
+	return this.each(function() {
+		//listen for scroll
+		var el = $(this);
+		el.hide(); //in case the user forgot
+		$(window).scroll(function() {
+			if($(window).scrollTop() >= settings.min)
+			{
+				el.fadeIn(settings.fadeSpeed);
+			}
+			else
+			{
+				el.fadeOut(settings.fadeSpeed);
+			}
+		});
+	});
+};
+
